476dd6fb3b54f14377aff0a11a74bc4ef97e2ef1
Heudes Xavier de Oliveira <heudesxavier@gmail.com>
2023-05-14 14:01:29 +0000

    recursive function

--- main.cpp
+++ main.cpp
@@ -10,21 +10,30 @@ InterruptIn center(D4);
 // Initialize a DigitalOut pin to control an LED
 DigitalOut led(LED1);
 
+// Recursive function to compute the nth term of the Fibonacci sequence
+int fibonacci(int n) {
+    if (n == 0) {  // Base case for n = 0
+        return 0;
+    } else if (n == 1) {  // Base case for n = 1
+        return 1;
+    } else {  // Recursive case
+        int prev1 = fibonacci(n - 1);
+        int prev2 = fibonacci(n - 2);
+        return prev1 + prev2;
+    }
+}
+
 int main() {
     
-    int n = 10;  // Compute first 10 terms of Fibonacci sequence
+    int n = 20;  // Compute first 10 terms of Fibonacci sequence
 
-    int a = 0, b = 1, c;  // Initialize first two terms of sequence
     for (int i = 0; i <= n; i++) {  // Loop through first n terms
-        printf("%d ", a);  // Print current term to console
+        int fib = fibonacci(i);  // Compute the ith term of the sequence
+        printf("%d ", fib);  // Print current term to console
         led = !led;  // Toggle LED on and off
-        c = a + b;  // Compute next term
-        a = b;  // Update previous terms
-        b = c;
         wait(0.5);  // Delay for half a second
     }
     printf("\n");  // Print newline character to console
 
 }
-
 
